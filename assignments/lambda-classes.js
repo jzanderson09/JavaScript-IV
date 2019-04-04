bccc0e044efe0a58477097d1c93cc6b8368cd097
jzanderson09 <jzanderson09@gmail.com>
2019-04-04 03:47:41 -0500

    All classes are completed with proper inheritance. Tested each one individually and all methods under each class work properlygit add .  Now just going to tidy up code and put all the console.logs at the bottom, ensuring that they all work correctly.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,4 @@
 // CODE here for your Lambda Classes
-
 /*
 Three different types of People:
 1) Instructors --> Extensions of "Person"
@@ -7,14 +6,7 @@ Three different types of People:
 3) Project Managers --> Extensions of Instructors
 */
 
-/*props: {
-    name:
-    age: 
-    location:
-    gender:
-}
-*/
-//Base class:
+//(Person) base class:
 class Person {
     constructor(props) {
         this.name = props.name;
@@ -28,6 +20,15 @@ class Person {
     }
 }
 
+//person ex:
+const dude = new Person({
+    name: 'Dude',
+    age: 40,
+    location: 'Pomona, CA',
+    gender: 'male'
+});
+
+//(Instructor) child class:
 class Instructor extends Person {
     constructor(props) {
         super(props);
@@ -39,11 +40,16 @@ class Instructor extends Person {
     demo(subject) {
         console.log(`Today we are learning about ${subject}!`);
     }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on {subject}`);
+    }
 }
 
-const jake = new Instructor({
-    name: 'Jake',
-    age: 27,
+//instructor ex:
+const kyle = new Instructor({
+    name: 'Kyle',
+    age: 28,
     location: 'Spearfish, SD',
     gender: 'male',
     specialty: 'CSS',
@@ -51,6 +57,88 @@ const jake = new Instructor({
     catchPhrase: "If you are not living on the edge, you are taking up too much room!"
 });
 
-console.log(jake);
-jake.speak();
-jake.demo('Javascript Fundamentals');
+//(Student) child class:
+class Student extends Person {
+    constructor(props) {
+        super(props);
+        this.previousBackground = props.previousBackground;
+        this.className = props.className;
+        this.favSubjects = props.favSubjects;
+    }
+
+    listsSubjects() {
+        this.favSubjects.forEach(arrItem => {
+            console.log(arrItem);
+        });
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}!`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}!`);
+    }
+}
+
+//student ex:
+const danny = new Student({
+    name: 'Danny',
+    age: 29,
+    location: 'Spearfish, SD',
+    gender: 'male',
+    specialty: 'SQL',
+    favLanguage: 'Blockchain',
+    catchPhrase: "Pitter patter, let's get at 'er!",
+    previousBackground: 'HTML, CSS',
+    className: 'WEBPT5',
+    favSubjects: ['History', 'Computer Science', 'Music', 'Art']
+});
+
+//(ProjectManager) child class:
+class ProjectManager extends Instructor {
+    constructor(props) {
+        super(props);
+        this.gradClassName = props.gradClassName;
+        this.favInstructor = props.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @${channel} standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}!`);
+    }
+}
+
+//ProjectManager ex:
+
+const cameron = new ProjectManager({
+    name: 'Cameron',
+    age: 31,
+    location: 'Salt Lake City, UT',
+    gender: 'male',
+    specialty: 'C++',
+    favLanguage: 'Python',
+    catchPhrase: "I got this!",
+    gradClassName: 'FSW2',
+    favInstructor: 'Josh Knell'
+});
+
+//student ex:
+const roman = new Student({
+    name: 'Nicholas',
+    age: 27,
+    location: 'Spearfish, SD',
+    gender: 'male',
+    specialty: 'UI/UX Design',
+    favLanguage: 'English ^_^',
+    catchPhrase: "What can ya do?",
+    previousBackground: 'HTML',
+    className: 'FSW18',
+    favSubjects: ['Music', 'P.E.', 'Geography']
+});
+
+cameron.standUp('FSW8');
+cameron.debugsCode(roman, 'Javascript Fundamentals');
