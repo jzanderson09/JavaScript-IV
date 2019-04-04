f164eae2a7cba08cf74ce0dc0ffd4d11810ddad2
jzanderson09 <jzanderson09@gmail.com>
2019-04-04 02:47:23 -0500

    Completed Person class on lambda-classes.js and tested with console.log --> Works!  On to the others.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,39 @@
 // CODE here for your Lambda Classes
+
+/*
+Three different types of People:
+1) Instructors --> Extensions of "Person"
+2) Students --> Extensions of "Person"
+3) Project Managers --> Extensions of Instructors
+*/
+
+/*props: {
+    name:
+    age: 
+    location:
+    gender:
+}
+*/
+//Base class:
+class Person {
+    constructor(props) {
+        this.name = props.name;
+        this.age = props.age;
+        this.location = props.location;
+        this.gender = props.gender;
+    }
+
+    speak () {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}!`);
+    }
+}
+
+const jake = new Person({
+    name: 'Jake',
+    age: 27,
+    location: 'Spearfish, SD',
+    gender: 'male'
+});
+
+console.log(jake);
+jake.speak();
